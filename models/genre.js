72e335b4c10dd5b0f1597b78de784df64890e78e
salamanderctesiphon <swbrookshire@gmail.com>
2023-11-16 13:01:16 -0600

    created the data models for the project

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const genreSchema = new Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
+})
+
+// Virtual for the genre's url
+genreSchema.virtual('url').get(function () {
+  return `/catalog/genre/${this._genre}`
+})
+
+module.exports = mongoose.model('genre', genreSchema)
